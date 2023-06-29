d4701a098c75cda2f01ff39d1e48b8cdca1f2fa8
DanielAtOcc <138094369+DanielAtOcc@users.noreply.github.com>
2023-06-29 11:57:46 -0700

    Master reference (#3)
    * Added body to mobile application
    
    * Added header to show ownership
    
    * Added text button to be integrated with Apiservice
    
    * Updated Header
    
    ---------
    
    Co-authored-by: Snippy <digity63@gmail.com>

--- Frontend/mobile/frontend_mobile_app/lib/main.dart
+++ Frontend/mobile/frontend_mobile_app/lib/main.dart
@@ -1,7 +1,9 @@
+/*
+  Mobile Leader: DanielAtOcc (digity63@gmail.com)
+*/
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(const RootApp());
 }
 
@@ -12,7 +14,20 @@ class RootApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      appBar: AppBar(title: Text("Welcome to the mobile app"),),
+        appBar: AppBar(
+          title: Text("Project Coast Edu"),
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center,mainAxisSize: MainAxisSize.max,
+            children: [
+              Center(
+                child: Text("This is the mobile application"),
+              ),
+              TextButton(onPressed: (){}, child: Text("Sign Up"))
+            ],
+          ),
+        ),
       ),
     );
   }
