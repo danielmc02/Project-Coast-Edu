d46616308b62d0e64c74555033bb473596c263aa
Snippy <70786357+danielmc02@users.noreply.github.com>
2023-11-13 20:40:19 -0800

    Good enough for now (#20)

--- Frontend/mobile/frontend_mobile_app/lib/models/boxes.dart
+++ Frontend/mobile/frontend_mobile_app/lib/models/boxes.dart
@@ -3,6 +3,10 @@ import 'package:hive/hive.dart';
 
 import 'user.dart';
 
+
+/*
+Static persisted data models
+*/
 class Boxes
 {
   static Box<User> getUserBox() => Hive.box<User>('mainUser');
--- Frontend/mobile/frontend_mobile_app/lib/theme/styles.dart
+++ Frontend/mobile/frontend_mobile_app/lib/theme/styles.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
-class Styles
-{
-    static const headerText2 = TextStyle(fontSize: 32,color: Colors.black, fontFamily: "Poppins", fontStyle: FontStyle.normal,);
+/*
+Styles for global texts
+*/
+class Styles {
 
-  static const headerText1 = TextStyle(fontSize: 48,color: Colors.black, fontFamily: "Poppins", fontStyle: FontStyle.normal,fontWeight: FontWeight.w900,);
-  static const buttonText = TextStyle(fontSize: 24,color: Colors.black, fontFamily: "Poppins", fontStyle: FontStyle.normal,fontWeight: FontWeight.bold);
-  static const buttonText2 = TextStyle(fontSize: 24,color: Colors.white, fontFamily: "Poppins", fontStyle: FontStyle.normal,fontWeight: FontWeight.bold);
-  static const chipText = TextStyle(fontSize: 34,color: Colors.black, fontFamily: "Poppins", fontStyle: FontStyle.normal,fontWeight: FontWeight.w600);
 
+  static const headerText2 = TextStyle(
+    fontSize: 32,
+    color: Colors.black,
+    fontFamily: "Poppins",
+    fontStyle: FontStyle.normal,
+  );
+
+  static const headerText1 = TextStyle(
+    fontSize: 48,
+    color: Colors.black,
+    fontFamily: "Poppins",
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w900,
+  );
+
+  static const buttonText = TextStyle(
+      fontSize: 24,
+      color: Colors.black,
+      fontFamily: "Poppins",
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.bold);
+
+  static const buttonText2 = TextStyle(
+      fontSize: 24,
+      color: Colors.white,
+      fontFamily: "Poppins",
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.bold);
+
+  static const chipText = TextStyle(
+      fontSize: 34,
+      color: Colors.black,
+      fontFamily: "Poppins",
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w600);
 }
